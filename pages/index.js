a2cc79478a46aa01b74e4f1fda354526701ff7da
sarthak sharma <sharma.sarthak222872@gmail.com>
2023-11-07 17:07:40 +0530

    Understand the authenthication in nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,31 @@
 import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
 import { Inter } from 'next/font/google';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const { data: session, status } = useSession();
+
   return (
     <div className={styles.container}>
-      <Image
-        src="https://nextjs.org/static/blog/next-14/twitter-card.png"
-        alt='loki image'
-        width={800}
-        height={400}
-      />
+      {status === 'loading' && <p>Loading...</p>}
+      {session && (
+        <>
+          <h1>Hello {session.user.name}</h1>
+          <Image
+            src="https://nextjs.org/static/blog/next-14/twitter-card.png"
+            alt='loki image'
+            width={800}
+            height={400}
+          />
+          <button onClick={() => signOut()}>Logout</button>
+        </>
+      )}
+      {!session && (
+        <button onClick={() => signIn()}>Login</button>
+      )}
     </div>
   );
 }
